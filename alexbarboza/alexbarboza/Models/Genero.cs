d4e7522cefbf7e6be28d410e5050f04b342a8839
assabarboza <alexplat11@hotmail.com>
2016-06-21 20:16:10 -0300

    Projeto PI

---
+++ alexbarboza/alexbarboza/Models/Genero.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace alexbarboza.Models
+{
+    [Table("genero")]
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "A destreve dete ter entre 5 a 50 caracteres")]
+        [Required]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        public List<Livro> Livros { get; set; }
+    }
+}
